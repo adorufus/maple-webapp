555a842e4e9b9b2874263f2648b59f0079826e80
Reingalrt <adorufus@gmail.com>
2023-05-30 12:44:58 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'maple-webapp'`, () => {
+  it(`should have as title 'maple-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('maple-webapp');
+    expect(app.title).toEqual('maple-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('maple-webapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('maple-web app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import {ScrollToModule} from "@nicky-lenaers/ngx-scroll-to";
+import {CarouselModule} from "ngx-owl-carousel-o";
+import {FeatherModule} from "angular-feather";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {allIcons} from "angular-feather/icons";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScrollToModule.forRoot(),
+    CarouselModule,
+    FeatherModule.pick(allIcons),
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
