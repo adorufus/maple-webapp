958a769c1690ee52a18fb2ce4ae8b9717ed45c05
irdam <muhammadirdam99@gmail.com>
2023-06-05 13:25:01 +0700

    commit

---
+++ src/app/core/components/deptstore/deptstore.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deptstore',
+  templateUrl: './deptstore.component.html',
+  styleUrls: ['./deptstore.component.scss']
+})
+export class DeptstoreComponent {
+
+}
